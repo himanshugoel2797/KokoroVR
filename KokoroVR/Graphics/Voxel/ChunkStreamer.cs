ca26d1adc92094e7e29fe19d04c1d8bc3f61ecde
Himanshu Goel <himanshu.goel2797@gmail.com>
2020-01-12 03:24:56 -0500

    Further cleanup.

--- KokoroVR/Graphics/Voxel/ChunkStreamer.cs
+++ KokoroVR/Graphics/Voxel/ChunkStreamer.cs
@@ -12,27 +12,18 @@ namespace KokoroVR.Graphics.Voxel
     {
         //Receive chunk faces
         public const int VRAMCacheSize = 1024;  //TODO make this depend on total available vram
-        const uint blk_cnt = 8192 * 8;
+        const uint blk_cnt = 8192 * 2;
 
         private Chunk[] ChunkList;
         private (ChunkMesh, int, double)[] ChunkCache;
-        private BufferGroupAllocator buffer;
         private ShaderStorageBuffer drawParams;
         private RenderQueue2 queue;
         private ShaderProgram voxelShader;
         private RenderState state;
         private List<(int, Vector3)> Draws;
 
-        //private ShaderProgram cullingShader;
-        //private ShaderProgram compactShader;
-
         private BufferAllocator indexBufferAllocator;
         private IndexBuffer indexBuffer;
-        //private ShaderStorageBuffer o_indexSSBO;
-        //private IndexBuffer o_indexBuffer;
-
-        private ShaderStorageBuffer multiDrawParams;
-        private ShaderStorageBuffer multiDrawParams2;
 
         private double cur_time;
         private VREye cur_eye;
@@ -48,11 +39,6 @@ namespace KokoroVR.Graphics.Voxel
 
             indexBufferAllocator = new BufferAllocator(ChunkConstants.BlockSize * sizeof(uint), blk_cnt, false, PixelInternalFormat.R32ui);
             indexBuffer = new IndexBuffer(indexBufferAllocator.BufferTex.Buffer, false);
-            //o_indexSSBO = new ShaderStorageBuffer(6 * 64 * 48 * blk_cnt * sizeof(uint), false);
-            //o_indexBuffer = new IndexBuffer(o_indexSSBO, false);
-
-            multiDrawParams = new ShaderStorageBuffer((5 * blk_cnt + 4) * sizeof(uint), true);
-            multiDrawParams2 = new ShaderStorageBuffer((5 * blk_cnt + 4) * sizeof(uint), false);
 
             MaxChunkCount = max_count;
             ChunkList = new Chunk[max_count];
@@ -68,9 +54,6 @@ namespace KokoroVR.Graphics.Voxel
             drawParams = new ShaderStorageBuffer(blk_cnt * 8 * sizeof(uint), false);
             voxelShader = new ShaderProgram(ShaderSource.Load(ShaderType.VertexShader, "Shaders/Deferred/Voxel/vertex.glsl"),
                                             ShaderSource.Load(ShaderType.FragmentShader, "Shaders/Deferred/Voxel/fragment.glsl"));
-
-            //cullingShader = new ShaderProgram(ShaderSource.Load(ShaderType.ComputeShader, "Shaders/Deferred/Voxel/Cull/filter.glsl"));
-            //compactShader = new ShaderProgram(ShaderSource.Load(ShaderType.ComputeShader, "Shaders/Deferred/Voxel/Cull/compact.glsl"));
 
             MaterialMap = new VoxelDictionary();
         }
@@ -96,6 +79,7 @@ namespace KokoroVR.Graphics.Voxel
         public void RenderChunk(Chunk c, Vector3 offset)
         {
             if (c.streamer != this) throw new Exception("Chunk not owned by current renderer.");
+            if (c.empty) return;
 
             //Don't proceed if this chunk isn't supposed to be visible
             if (!Engine.Frustums[(int)cur_eye].IsVisible(new Vector4(offset - Vector3.One * ChunkConstants.Side * 0.5f, (float)(ChunkConstants.Side * 0.75f * System.Math.Sqrt(3)))))
@@ -120,11 +104,11 @@ namespace KokoroVR.Graphics.Voxel
                 ChunkCache[lru].Item2 = c.id;
 
                 //if (c.faces == null) c.RebuildFullMesh();
-                if (!c.empty) c.update_pending = true;
+                c.update_pending = true;
             }
 
             //Upload the current mesh state if it has been updated recently
-            if (!c.empty && c.update_pending)
+            if (c.update_pending)
             {
                 unsafe
                 {
@@ -148,12 +132,7 @@ namespace KokoroVR.Graphics.Voxel
                 c.norm_mask = null;
                 c.update_pending = false;
             }
-
-            //TODO Sort these draws front to back
-            //TODO do the same internally for each mesh
-            //Record this chunk's draw
-            if (!c.empty)
-                Draws.Add((mesh_idx, offset - Vector3.One * ChunkConstants.Side * 0.5f));
+            Draws.Add((mesh_idx, offset - Vector3.One * ChunkConstants.Side * 0.5f));
         }
 
         public override void Update(double time, World parent)
@@ -168,39 +147,6 @@ namespace KokoroVR.Graphics.Voxel
         {
             cur_time += time;
             cur_eye = eye;
-
-            unsafe
-            {
-                var mp_p = (uint*)multiDrawParams.Update();
-                mp_p[0] = blk_cnt / 64;
-                mp_p[1] = 1;
-                mp_p[2] = 1;
-                mp_p[3] = 0;
-                for (int i = 0; i < blk_cnt; i++)
-                {
-                    mp_p[i * 5 + 4] = 0;
-                    mp_p[i * 5 + 5] = 1;
-                    mp_p[i * 5 + 6] = (uint)(i * ChunkConstants.BlockSize);
-                    mp_p[i * 5 + 7] = 0;
-                    mp_p[i * 5 + 8] = 0;
-                }
-                multiDrawParams.UpdateDone();
-
-                var mp_p = (uint*)multiDrawParams2.Update();
-                mp_p[0] = blk_cnt / 64;
-                mp_p[1] = 1;
-                mp_p[2] = 1;
-                mp_p[3] = 0;
-                for (int i = 0; i < blk_cnt; i++)
-                {
-                    mp_p[i * 5 + 4] = 0;
-                    mp_p[i * 5 + 5] = 1;
-                    mp_p[i * 5 + 6] = (uint)(i * ChunkConstants.BlockSize);
-                    mp_p[i * 5 + 7] = 0;
-                    mp_p[i * 5 + 8] = 0;
-                }
-                multiDrawParams2.UpdateDone();
-            }
 
             Draws.Clear();
             voxelShader.Set("eyePos", Engine.CurrentPlayer.Position);
@@ -237,25 +183,6 @@ namespace KokoroVR.Graphics.Voxel
                 }
 
                 parent.queue.EndRecording(Engine.Frustums[(int)eye], Engine.CurrentPlayer.Position);
-
-                /*parent.cullingShader.Set("eyePos", Engine.CurrentPlayer.Position);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.drawParams, 1);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.indexBuffer.Buffer, 2);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.queue.MultidrawParams, 3);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.o_indexSSBO, 4);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.multiDrawParams, 5);
-                GraphicsDevice.SetShaderStorageBufferBinding(parent.multiDrawParams2, 6);
-                GraphicsDevice.DispatchIndirectSyncComputeJob(parent.cullingShader, parent.queue.MultidrawParams, 0);
-                parent.multiDrawParams.UpdateDone();
-                GraphicsDevice.DispatchIndirectSyncComputeJob(parent.compactShader, parent.multiDrawParams, 0);
-                parent.multiDrawParams2.UpdateDone();
-                parent.o_indexBuffer.UpdateDone();
-                
-                GraphicsDevice.SetRenderState(parent.state);
-                GraphicsDevice.SetMultiDrawParameterBuffer(parent.multiDrawParams2);
-                GraphicsDevice.SetParameterBuffer(parent.multiDrawParams2);
-                GraphicsDevice.MultiDrawIndirectCount(PrimitiveType.Triangles, parent.multiDrawParams2.Offset + sizeof(uint) * 4, parent.multiDrawParams2.Offset + sizeof(uint) * 3, blk_cnt, true, false);
-                */
                 parent.queue.Submit();
             }
 
