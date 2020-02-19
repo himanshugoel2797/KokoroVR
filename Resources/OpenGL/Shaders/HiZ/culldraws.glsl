1f448f04579283b802cc9ce0eb4f5355ccbb3232
Himanshu Goel <himanshu.goel2797@gmail.com>
2020-02-19 13:47:26 -0500

    Clean up culldraw.glsl

--- Resources/OpenGL/Shaders/HiZ/culldraws.glsl
+++ Resources/OpenGL/Shaders/HiZ/culldraws.glsl
@@ -41,7 +41,7 @@ layout(std140, binding = 2) uniform HiZLayers_t {
     uvec4 mip_tex;
 } HiZLayers;
 
-layout(std430, binding = 2) restrict buffer DrawCMDs_t {
+layout(std430, binding = 2) restrict coherent buffer DrawCMDs_t {
     uint drawCount;
     uint p0;
     uint p1;
@@ -56,31 +56,23 @@ void main(){
     int drawIdx = int(gl_GlobalInvocationID.x);
 
     //Compute screen space position of each AABB corner given the previous VP matrix
-    vec3 aabb_min = src_DrawCMDs.cmds[drawIdx].aabb_min.xyz;
-    vec3 aabb_max = src_DrawCMDs.cmds[drawIdx].aabb_max.xyz;
-    vec4 persp_v[8];
-
-    persp_v[0] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_min.x, aabb_min.y, aabb_min.z, 1);
-    persp_v[1] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_min.x, aabb_min.y, aabb_max.z, 1);
-    persp_v[2] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_min.x, aabb_max.y, aabb_min.z, 1);
-    persp_v[3] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_min.x, aabb_max.y, aabb_max.z, 1);
-    persp_v[4] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_max.x, aabb_min.y, aabb_min.z, 1);
-    persp_v[5] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_max.x, aabb_min.y, aabb_max.z, 1);
-    persp_v[6] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_max.x, aabb_max.y, aabb_min.z, 1);
-    persp_v[7] = GlobalParams.prev_vp[eyeIdx] * vec4(aabb_max.x, aabb_max.y, aabb_max.z, 1);
+    vec3 aabb[2];
+    aabb[0] = src_DrawCMDs.cmds[drawIdx].aabb_min.xyz;
+    aabb[1] = src_DrawCMDs.cmds[drawIdx].aabb_max.xyz;
+    vec4 persp_v = GlobalParams.prev_vp[eyeIdx] * vec4(aabb[0].x, aabb[0].y, aabb[0].z, 1);
+    persp_v.xyz /= persp_v.w;
     
-    persp_v[0].xyz /= persp_v[0].w;
-
     vec2 rect[2];
     float inst_dpth;
-    rect[0] = persp_v[0].xy;
-    rect[1] = persp_v[0].xy;
-    inst_dpth = persp_v[0].z;
+    rect[0] = persp_v.xy;
+    rect[1] = persp_v.xy;
+    inst_dpth = persp_v.z;
     for (int i = 1; i < 8; i++){
-        persp_v[i].xyz /= persp_v[i].w;
-        rect[0] = min(rect[0], persp_v[i].xy);
-        rect[1] = max(rect[1], persp_v[i].xy);
-        inst_dpth = max(inst_dpth, persp_v[i].z);
+        persp_v = GlobalParams.prev_vp[eyeIdx] * vec4(aabb[i >> 2].x, aabb[(i >> 1) & 1].y, aabb[i & 1].z, 1);
+        persp_v.xyz /= persp_v.w;
+        rect[0] = min(rect[0], persp_v.xy);
+        rect[1] = max(rect[1], persp_v.xy);
+        inst_dpth = max(inst_dpth, persp_v.z);
     }
     rect[0] = rect[0] * 0.5f + 0.5f;
     rect[1] = rect[1] * 0.5f + 0.5f;
@@ -94,12 +86,7 @@ void main(){
     samples.y = textureLod(hiz_sampler, vec2(rect[0].x, rect[1].y), lod).x;
     samples.z = textureLod(hiz_sampler, vec2(rect[1].x, rect[1].y), lod).x;
     samples.w = textureLod(hiz_sampler, vec2(rect[1].x, rect[0].y), lod).x;
-    //image2D hiz = image2D(HiZLayers.mips[0][int(lod)].xy);
-    //ivec2 im_sz = imageSize(hiz);
-    //samples.x = imageLoad(hiz, ivec2(rect[0].x * im_sz.x, rect[0].y * im_sz.y)).x;
-    //samples.y = imageLoad(hiz, ivec2(rect[0].x * im_sz.x, rect[1].y * im_sz.y)).x;
-    //samples.z = imageLoad(hiz, ivec2(rect[1].x * im_sz.x, rect[1].y * im_sz.y)).x;
-    //samples.w = imageLoad(hiz, ivec2(rect[1].x * im_sz.x, rect[0].y * im_sz.y)).x;
+    
     float sampledDepth = min(min(samples.x, samples.y), min(samples.z, samples.w));
-    src_DrawCMDs.cmds[drawIdx].count *= (inst_dpth >= sampledDepth) ? 1 : 0;
+    src_DrawCMDs.cmds[drawIdx].count *= int(inst_dpth >= sampledDepth);
 }
